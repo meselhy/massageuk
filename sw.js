a3796a191fefae4a678c61be5d81d18f2d49623d
Abdalla Meselhy <abdallameselhy@yahoo.com>
2024-06-01 18:52:06 +0300

    updating icons

--- sw.js
+++ sw.js
@@ -1,5 +1,24 @@
-const CACHE_NAME = 'cool-cache';
+// This is the "Offline copy of pages" service worker
+const CACHE = "off-cache";
 
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
+
+
+
+const CACHE_NAME = 'cool-cache';
 // Add whichever assets you want to pre-cache here:
 const PRECACHE_ASSETS = [
     '/index.html',
@@ -40,23 +59,3 @@ self.addEventListener('fetch', event => {
       };
   });
 });
-
-// This is the "Offline copy of pages" service worker
-
-const CACHE = "off-cache";
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
-
-workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
-  })
-);
-
